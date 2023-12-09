0b3120f964273066b241cf727d054185d5eb3e1e
Michael Xie <mxie0114@glamil.com>
2023-12-09 14:12:00 -0500

    restart

--- server/database.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import 'dotenv/config'
-// Create a single supabase client for interacting with your database
-const supabase = createClient(process.env.dbURL, process.env.dbAPI);
-
-
-export default supabase;

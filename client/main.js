311d93c0c696424fec99015c3c721fc7112b1ac8
Michael Xie <mxie0114@glamil.com>
2023-12-14 11:26:54 -0500

    Fixing some odd behavior with debug

--- client/main.js
+++ client/main.js
@@ -3,17 +3,17 @@
 const tName = document.getElementById("tNameInput");
 const ListInput = document.getElementById("Einput");
 const reset = document.getElementById("reset");
-//const Add = document.getElementById("submitL");
 const submit  = document.getElementById("submit");
 const list = document.getElementById("list");
-//const tList = document.getElementById("tourneyList");
-//const eList = document.getElementById("EntrantsList");
+
 const ppl = {};
 const matches = {};
 const places = {};
 let count = 0;
-if(localStorage.length != 0){
+if(localStorage.length != 0 && !localStorage.key(0) == "debug"){
     document.getElementById("inputForm").style.display = "none";
+}else{
+    document.getElementById("inputForm").style.display = "visible";
 }
 //Adding event listeners for the buttons
 submit.addEventListener("click", ()=>{
@@ -23,7 +23,6 @@ submit.addEventListener("click", ()=>{
     var entry = document.createElement('li');
     entry.innerHTML += ""
     entry.appendChild(document.createTextNode(tourney));
-    //tList.appendChild(entry);
     tName.value = "";
     document.getElementById("inputForm").style.display = "none";
 });
@@ -32,7 +31,6 @@ function addEntrants(){
     let entrants = list.value;
     entrants = entrants.split(',');
     entrants = entrants.map(e=> e.trim())
-    console.log(entrants);
     for(i in entrants){
         ppl[count++] = entrants[i];
     }

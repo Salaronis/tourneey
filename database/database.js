7c441adf6d9e9f30ab2a27b4c138af247be3141a
Michael Xie <mxie0114@glamil.com>
2023-12-14 00:26:08 -0500

    We're getting somewhere? Added majority of HTML and working on main.js

--- database/database.js
+++ database/database.js
@@ -2,15 +2,23 @@ import { createClient } from '@supabase/supabase-js'
 import 'dotenv/config'
 
 
-const supabase = createClient(process.env.dbURL, process.env.dbAPI);
 
+const supabase = createClient(process.env.dbURL, process.env.dbAPI);
 /*
 async function getTournaments(user){
+    
     const {data , error} = await supabase.from('tournaments').select().eq('User' , user);
 }
 
 async function getParticipants(tournament){
     const {data, error} = await supabase.from('participants').select('name').eq("tournament" , tournament);
+
+}
+
+async function AddParticipant(tournament , user){
+    const {data,error} = await supabase.from('Entrants')
+    localStorage.setItem(tournament ,);
 }
 */
+
 export default supabase;
--- main.js
+++ main.js
@@ -1,34 +1,15 @@
 
-/*import { createAuth0Client } from "@auth0/auth0-spa-js";
-import 'dotenv/config'
-const auth0  = createAuth0Client({
-    domain: process.env.authDomain,
-    clientId: process.env.authID,
-    authorizationParams: {
-      redirect_uri: window.location.origin
-    }
-  });
+const form = document.getElementById("fButton");
+const normInput = document.getElementById("input1");
+const ListInput = document.getElementById("input2");
+const submit  = document.getElementById("submit")
 
+form.addEventListener("click", ()=>{
+    
+});
 
-const loginButton = document.getElementById("login");
-  loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    auth0Client.loginWithRedirect();
-});  
+submit.addEventListener("click", ()=>{
 
-if (location.search.includes("state=") && 
-      (location.search.includes("code=") || 
-      location.search.includes("error="))) {
-    await auth0Client.handleRedirectCallback();
-    window.history.replaceState({}, document.title, "/");
-    }
+});
 
-const logout = () => {
-  auth0Client.logout({
-    logoutParams: {
-      returnTo: window.location.origin
-    }
-  });
-};
-*/
-  
+ListInput.style.display = "none";

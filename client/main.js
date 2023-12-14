0f6288a6ea28d4d68418a68b86d9a7077e4480cc
Michael Xie <mxie0114@glamil.com>
2023-12-14 18:07:18 -0500

    Something is happening lol

--- client/main.js
+++ client/main.js
@@ -1,30 +1,67 @@
-import {bracket} from './bracket.js'
+//Temporary Fix?
+class bracket{
+    constructor(){
+        this.entrants = {};
+        this.placement = {};
+        this.size = 0;
+        this.entrantAMT = 0;
+    }
+    getEntrants(){
+        return this.entrants;
+    }
+    setSize(){
+        this.size = Math.log2(size) % 1 === 0
+    }
+    render(element){
+        element.innerHTML = '';
+        for(let i = 0; i < this.size; i++){
+            //add div and spam out columns so that it looks like a bracket
+            const div = document.createElement('div');
+            div.classList.add('')
+            div.innerText = this.entrants[i];
+            element.appendChild(div);
+        }{
+            
+        }
+    }
+
+    placeRemove(entrant){
+        this.placement[entrantAMT--] = entrant;
+    }
+
+    reset(){
+        this.entrants = {};
+    }
+}
 //Adding references to index.html
+
 const tName = document.getElementById("tNameInput");
 const reset = document.getElementById("reset");
 const submit  = document.getElementById("submit");
 const list = document.getElementById("list");
 const main = document.getElementById("inputForm");
 const brack = document.getElementsByClassName("match");
-
+const name = document.getElementById("name");
+const b = new bracket();
 const ppl = {};
 const matches = {};
 const places = {};
 let count = 0;
-if(localStorage.length != 0 && !localStorage.key(0) == "debug"){
-    main.style.display = "none";
+ 
+if(localStorage.length >=1){
+    if(localStorage.key(0) != "debug" || localStorage.length != 1){
+        main.style.display = "none";
+    }
 }else{
-    main.style.display = "visible";
+    main.style.display = "block";
 }
+
 //Adding event listeners for the buttons
 submit.addEventListener("click", ()=>{
     addEntrants();
     let tourney = tName.value;
-    console.log(tourney);
-    var entry = document.createElement('li');
-    entry.innerHTML += ""
-    entry.appendChild(document.createTextNode(tourney));
     tName.value = "";
+    name.value = tourney;
     main.style.display = "none";
     
 });
@@ -33,11 +70,11 @@ function addEntrants(){
     entrants = entrants.split(',');
     entrants = entrants.map(e=> e.trim())
     for(i in entrants){
-        bracket.entrants[count] = entrants[i];
+        b.entrants[count] = entrants[i];
         ppl[count++] = entrants[i];
         
     }
-    bracket.entrantAMT  = count;
+    b.entrantAMT  = count;
     list.value = "";
     console.log(ppl);
     
@@ -48,5 +85,10 @@ reset.addEventListener("click" , ()=>{
     document.getElementById("inputForm").style.display = "block";
     localStorage.clear();
 });
+
+
+
+
+
 
 

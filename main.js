1a0dfc626225f0253d79a3e45c4d720f4e890ebe
Michael Xie <mxie0114@glamil.com>
2023-12-14 02:57:33 -0500

    Good Progress, But too ambitious for now

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
 
 //Adding references to index.html
 const tName = document.getElementById("tNameInput");
-const ListInput = document.getElementById("Einput");
-const Add = document.getElementById("submitL");
-const submit  = document.getElementById("submit");
+//const ListInput = document.getElementById("Einput");
+//const Add = document.getElementById("submitL");
+//const submit  = document.getElementById("submit");
 const list = document.getElementById("list");
+//const tList = document.getElementById("tourneyList");
+//const eList = document.getElementById("EntrantsList");
 
 //Adding event listeners for the buttons
 submit.addEventListener("click", ()=>{
+    addEntrants();
     let tourney = tName.value;
     console.log(tourney);
+    var entry = document.createElement('li');
+    entry.innerHTML += ""
+    entry.appendChild(document.createTextNode(tourney));
+    tList.appendChild(entry);
+    document.getElementById("view").innerText = tourney;
+    document.getElementById("view").style.visibility = "visible";
     tName.value = "";
-    
+    document.getElementsByClassName("center").style.display = "none";
 });
-
-ListInput.addEventListener("click" , ()=>{
+/*
+function addEntrants(){
     let entrants = list.value;
     entrants = entrants.split(',');
     entrants = entrants.map(e=> e.trim())
     console.log(entrants);
+    for(i in entrants){
+        let entry = document.createElement('li');
+        entry.innerHTML += ""
+        entry.appendChild(document.createTextNode(entrants[i]));
+        eList.appendChild(entry);
+    }
     list.value = "";
+}
+*/
+ListInput.addEventListener("click" , ()=>{
+    addEntrants();
 });
 
 
